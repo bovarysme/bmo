a12e682aa8113ffbaf57e5a9e760c92b96c0ace8
bovarysme <bovarysme@users.noreply.github.com>
2018-04-17 23:21:45 +0200

    Render the window badly

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -27,7 +27,7 @@ const (
 	OBJEnable
 	OBJSize
 	BGTileMapAddress
-	BGTileDataAddress
+	BGWindowTileDataAddress
 	WindowEnable
 	WindowTileMapAddress
 	LCDEnable
@@ -138,75 +138,92 @@ func (p *PPU) updateLine(line byte) {
 	}
 }
 
-// TODO: clean up
+// TODO: clean up, rename
+func (p *PPU) transferBGOrWindow(mapMask, line, mapLine byte, mapColumn, start int) {
+	const (
+		tileWidth    = 8
+		tileHeight   = 8
+		tileMapWidth = 32
+	)
+
+	var dataAddress, mapAddress uint16
+	if p.getFlag(LCDC, BGWindowTileDataAddress) {
+		dataAddress = 0x8000
+	} else {
+		dataAddress = 0x8800
+	}
+
+	if p.getFlag(LCDC, mapMask) {
+		mapAddress = 0x9c00
+	} else {
+		mapAddress = 0x9800
+	}
+
+	// Tiles are 8 lines tall and maps 32 tiles wide (with one tile being
+	// one byte)
+	mapOffset := mapAddress + uint16(mapLine)/tileHeight*tileMapWidth
+
+	paletteData := p.mmu.ReadByte(BGP)
+	var palette [4]byte
+	for i := uint(0); i < 4; i++ {
+		palette[i] = paletteData >> (i * 2) & 0x3
+	}
+
+	for i := start; i < ScreenWidth; i += tileWidth {
+		// XXX
+		var temp byte = byte(i) + byte(mapColumn)
+		address := mapOffset + uint16(temp)/tileWidth
+
+		var tileNumber uint16
+		if dataAddress == 0x8800 {
+			tileNumber = uint16(int8(p.mmu.ReadByte(address))) + 128
+		} else {
+			tileNumber = uint16(p.mmu.ReadByte(address))
+		}
+
+		// Tile data = 16 bytes
+		dataOffset := dataAddress + tileNumber*16 + uint16(mapLine)%tileHeight*2
+		tileData := p.mmu.ReadWord(dataOffset)
+
+		high := byte(tileData >> 8)
+		low := byte(tileData & 0xff)
+
+		for j := 0; j < tileWidth; j++ {
+			x := i + j
+			if x >= ScreenWidth {
+				break
+			}
+
+			colorNumber := high>>(7-byte(j))&1<<1 | low>>(7-byte(j))&1
+			color := Colors[palette[colorNumber]]
+
+			index := Pitch*int(line) + ColorDepth*x
+			p.Pixels[index] = color[0]
+			p.Pixels[index+1] = color[1]
+			p.Pixels[index+2] = color[2]
+		}
+	}
+}
+
 func (p *PPU) transferLine(line byte) {
 	if p.getFlag(LCDC, BGEnable) {
-		var dataAddress uint16
-		if p.getFlag(LCDC, BGTileDataAddress) {
-			dataAddress = 0x8000
-		} else {
-			dataAddress = 0x8800
-		}
-
-		var mapAddress uint16
-		if p.getFlag(LCDC, BGTileMapAddress) {
-			mapAddress = 0x9c00
-		} else {
-			mapAddress = 0x9800
-		}
-
 		mapLine := line + p.mmu.ReadByte(SCY)
-		mapCol := p.mmu.ReadByte(SCX)
+		mapCol := int(p.mmu.ReadByte(SCX))
 
-		// Tiles are 8 lines tall and maps 32 tiles wide (with one tile being
-		// one byte)
-		mapOffset := mapAddress + uint16(mapLine)/8*32
+		p.transferBGOrWindow(BGTileMapAddress, line, mapLine, mapCol, 0)
+	}
 
-		paletteData := p.mmu.ReadByte(BGP)
-		var palette [4]byte
-		for i := uint(0); i < 4; i++ {
-			palette[i] = paletteData >> (i * 2) & 0x3
-		}
-
-		// The screen is 20 tiles wide
-		for i := 0; i < 20; i++ {
-			var temp byte = (byte(i)*8 + mapCol) / 8
-			address := mapOffset + uint16(temp)
-
-			var tileNumber uint16
-			if dataAddress == 0x8800 {
-				tileNumber = uint16(int8(p.mmu.ReadByte(address))) + 128
-			} else {
-				tileNumber = uint16(p.mmu.ReadByte(address))
-			}
-
-			// Tile data = 16 bytes
-			dataOffset := dataAddress + tileNumber*16 + uint16(mapLine)%8*2
-			tileData := p.mmu.ReadWord(dataOffset)
-
-			high := byte(tileData >> 8)
-			low := byte(tileData & 0xff)
-
-			for j := 0; j < 8; j++ {
-				colorNumber := high>>(7-byte(j))&1<<1 | low>>(7-byte(j))&1
-				color := Colors[palette[colorNumber]]
-
-				x := i*8 + j
-
-				index := Pitch*int(line) + ColorDepth*x
-				p.Pixels[index] = color[0]
-				p.Pixels[index+1] = color[1]
-				p.Pixels[index+2] = color[2]
-			}
+	if p.getFlag(LCDC, WindowEnable) {
+		wy := p.mmu.ReadByte(WY)
+		if line >= wy {
+			mapLine := line - wy
+			start := int(p.mmu.ReadByte(WX) - 7)
+			p.transferBGOrWindow(WindowTileMapAddress, line, mapLine, -start, start)
 		}
 	}
 
 	if p.getFlag(LCDC, OBJEnable) {
-		// TODO
-	}
-
-	if p.getFlag(LCDC, WindowEnable) {
-		// TODO
+		// TODO: render sprites
 	}
 }
 
