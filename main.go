9f1fe567e592dbe583c0ca65d8b737bee6ce3b44
bovarysme <bovarysme@users.noreply.github.com>
2018-04-30 21:47:41 +0200

    Implement CPU profiling

--- main.go
+++ main.go
@@ -3,26 +3,44 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"runtime/pprof"
 
 	"github.com/bovarysme/bmo/beemo"
 	"github.com/bovarysme/bmo/debug"
 )
 
 var debugFlag bool
+var screenScale int
+var cpuprofile string
 var bootromPath string
 var romPath string
-var screenScale int
 
 func init() {
 	flag.BoolVar(&debugFlag, "debug", false, "run the emulator in debug mode")
+	flag.IntVar(&screenScale, "scale", 2, "screen scale factor")
+	flag.StringVar(&cpuprofile, "cpuprofile", "", "write a CPU profile")
 	flag.StringVar(&romPath, "rom", "", "path to the ROM file")
 	flag.StringVar(&bootromPath, "bootrom", "roms/bootrom.gb", "path to the bootrom file")
-	flag.IntVar(&screenScale, "scale", 2, "screen scale factor")
 
 	flag.Parse()
 }
 
 func main() {
+	if cpuprofile != "" {
+		file, err := os.Create(cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = pprof.StartCPUProfile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer pprof.StopCPUProfile()
+	}
+
 	bmo, err := beemo.NewBMO(bootromPath, romPath, screenScale)
 	if err != nil {
 		log.Fatal(err)
